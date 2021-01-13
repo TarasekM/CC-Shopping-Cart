e64813f2240c448c2166eacd8a391c2f97b7e935
TarasekM <rezkth@gmail.com>
2021-01-13 13:50:40 +0100

    Add template for shopping cart.

---
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './ShoppingCart.module.scss';
+
+const ShoppingCart = (props) => {
+    <div className={styles.ShoppingCart}>
+        <div className={styles.HeaderContainer}>
+
+        </div>
+        <div className={styles.MainContainer}>
+
+        </div>
+    </div>
+}
+
+export default ShoppingCart;
