fa6a96d79b199e5cfbeff00879f072f78d6cf567
TarasekM <rezkth@gmail.com>
2021-01-13 16:07:06 +0100

    Implement Main Card element.

--- src/components/ShoppingCart/MainCard/MainCard.js
+++ src/components/ShoppingCart/MainCard/MainCard.js
@@ -1,9 +1,27 @@
-import React from 'react';
-
 import styles from './MainCard.module.scss';
 
+import Button from './../../Buttons/Button';
+
 const MainCard = (props) => {
-    
+    return(
+        <div className={styles.MainCard}>
+            <div className={styles.MainCardHeader}>
+                <div className={styles.TableHeader}>
+                    <p>{props.mainCardText.productName}</p>
+                    <p>{props.mainCardText.unitPrice}</p>
+                    <p>{props.mainCardText.quantity}</p>
+                </div>
+            </div>
+
+            <div className={styles.UpdateButtonContainer}>
+                <Button 
+                    text={props.mainCardText.updateButtonText}
+                    additionalStyle={styles.Bold}
+                    />
+            </div>
+
+        </div>
+    )
 }
 
 export default MainCard;
