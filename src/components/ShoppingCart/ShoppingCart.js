969a62f4ec08894c672dcd330e7323eefc8ca081
TarasekM <rezkth@gmail.com>
2021-01-13 15:29:44 +0100

    Add header section to the shopping cart.

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 import styles from './ShoppingCart.module.scss';
 
+// components
+import Button from './../Buttons/Button';
+import MainCard from './MainCard/MainCard';
+
+
 const ShoppingCart = (props) => {
-    <div className={styles.ShoppingCart}>
-        <div className={styles.HeaderContainer}>
-
+    return(
+        <div className={styles.ShoppingCart}>
+            <div className={styles.HeaderContainer}>
+                <div className={styles.Title}>
+                    {props.header.title}
+                </div>
+                <Button text={props.header.buttonText}/>
+            </div>
+            <div className={styles.MainContainer}>
+            </div>
         </div>
-        <div className={styles.MainContainer}>
-
-        </div>
-    </div>
+    );
 }
 
 export default ShoppingCart;
