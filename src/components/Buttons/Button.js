efbd2f52c8d94a4aba38a905fc0c92882b92eaf1
TarasekM <rezkth@gmail.com>
2021-01-13 15:08:53 +0100

    Add onclick function to the template of Button.

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -5,7 +5,7 @@ import styles from './Button.module.scss';
 const Button = (props) => {
     return(
         <div className={styles.ShoppingCartButton}>
-            <button>
+            <button onClick={props.onClickFunction}>
                 {props.text}
             </button>
         </div>
