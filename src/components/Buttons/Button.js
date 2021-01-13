beab66cd76945b688c6026d138f3be19d47592fc
TarasekM <rezkth@gmail.com>
2021-01-13 16:05:43 +0100

    Button now can get additional styling from parent.

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -3,9 +3,15 @@ import React from 'react';
 import styles from './Button.module.scss';
 
 const Button = (props) => {
+    const buttonStyle = (additionalStyle) => {
+        if (!additionalStyle){
+            return styles.ShoppingCartButton;
+        }
+        return `${styles.ShoppingCartButton} ${additionalStyle}`;
+    }
     return(
-        <div className={styles.ShoppingCartButton}>
-            <button onClick={props.onClickFunction}>
+        <div className={buttonStyle(props.additionalStyle)}>
+            <button className={styles.Button} onClick={props.onClickFunction}>
                 {props.text}
             </button>
         </div>
