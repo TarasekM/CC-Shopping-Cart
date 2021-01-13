7022a4fd36bde3d9b97c1c5e09add241acffe962
TarasekM <rezkth@gmail.com>
2021-01-13 15:28:59 +0100

    Add mock for language pack.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,19 @@
 import './App.css';
 
 function App() {
+  const languagePack = {
+    header: {
+      title: "Shopping Cart",
+      buttonText: "Proceed to checkout"
+    },
+    mainCard: {
+      productName: 'Product Name',
+      unitPrice: 'Unit Price',
+      quantity: 'Qty',
+      updateButtonText: 'Update Shopping Cart'
+    }
+  }
+
   return (
     <div className="App">
 
